8a5d64c1cc2117f1ed79bf5b089d18dbdd42568f
drubb <info@drubb.de>
2017-06-16 12:16:24 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,122 @@
-# lxw
-Simple cli wrapper for LXD containers
+# LXW - Simple cli wrapper for LXD containers
+
+## Introduction
+
+### About LXD/LXC
+Linux containers are great for local development environments, quickly testing stuff, and much more.
+Nowadays everybody talks Docker, but Docker might be an overkill in terms of complexity and learning curve.
+There's a lightweight alternative: Linux containers built with [LXD](https://linuxcontainers.org/lxd).
+
+There are two major differences between Docker containers and LXD containers:
+
+* LXD containers are persistent by default. Things you change inside containers are kept.
+* Docker containers normally run a single process. LXD doesn't have this limitation.
+
+In fact, LXD container are much more like traditional VMs. They are a good bet e.g. for simple
+local development environments. There are caveats for sure:
+
+* The ecosystem is much smaller, e.g. there's a lack of orchestration tools
+* The technology is still young, and might be a moving target. 
+
+### Why LXW ?
+LXW is a simple wrapper for LXD's command line interface, LXC. It's no replacement, but a small addition
+to make some things simpler, e.g. mounting host folders inside containers, and sharing file access rights.
+In fact, it just uses plain LXC commands and some shell script magic to achieve this goal.
+
+### Basic concept
+LXW uses preconfigured LXD images to spin up containers. So for example you might use a LXD stock image,
+start a container, orchestrate it for your needs and publish it as a new local image. So you might end up
+with some local base images like 'drupal-7', 'nodejs', whatever.
+
+That's the point where LXW comes in:
+
+Using your custom images, you'll spin up new containers for your projects in no time. LXW uses your current
+folder as container name, so it might go like this:
+
+* Your project is located in ~/projects/someproject
+* The source files are located in ~/projects/someproject/source
+* You want to use a customized local image named 'nodejs' 
+* Inside your folder, just create a new container using LXW:`lxw create nodejs`, this
+gives you a container named "someproject"
+* Share your source folder using LXW: `lxw mount code ./source /var/www/html`
+* Start the container: `lxw start`
+* Open a shell:`lxw shell`
+
+And so on, you get it.
+
+## Requirements
+The wrapper scripts needs [LXD](https://linuxcontainers.org/lxd) installed and configured.
+While it might work on many Linux distributions, it has been tested on current Ubuntu releases only.
+Stock images for LXD containers are located on the [LXD image server](https://us.images.linuxcontainers.org/)
+It's your part to create suitable custiom images for use with LXW.
+
+## Installation
+
+### LXD/LXC
+Instructions for installing LXD can be found [here](https://linuxcontainers.org/lxd/getting-started-cli/)
+
+### LXW
+Installing LXW is simple: just copy the script to /usr/local/bin, naming it 'lxw' and make it executable:
+
+```
+sudo mv lxw.sh /user/local/bin/lxw
+sudo chmod +x /usr/local/bin/lxw
+```
+
+## Commands
+
+### lxw create <image>
+Create a container using a local or remote image
+
+Examples:
+
+```
+lxw create images:alpine/3.5
+lxw create mylocalimage
+```
+
+### lxw start
+Start the container
+
+### lxw restart
+Restart the container
+
+### lxw stop
+Stop (shutdown) the container
+
+### lxw kill
+Destroy the container. Displays a confirmation message and deletes the container. All data outside of shared folders will get lost.  
+
+### lxw mount <alias> <host_path> <container_path>
+Share a folder on the host system with the container. Both the host user and the user inside the container get full access rights. Arguments:
+
+*alias* - a custom name for the share, as there can be multiple ones
+*host_path* - The path on the host system (relative or absolute)
+*container_path* - The path inside the container (must be absolute)
+
+Example:
+
+```
+lxw mount code ./source /var/www/html
+```
+
+### lxw unmount <alias>
+
+Unmounts a shared folder using the its alias
+
+Example:
+
+```
+lxw unmount code
+```
+
+### lxw shell
+Open a shell inside the container, using the default shell configured inside the container.
+
+### lxw status
+Display the current status of the container, including the IP
+
+### lxw help
+Show usage instructions
+
+## License: MIT
---
+++ lxw.sh
@@ -0,0 +1,171 @@
+#!/bin/sh
+
+set -e
+
+# Create a new container using a specified image.
+# Use the current folder name as container name.
+# Map the host users uid/gid to the container user.
+create(){
+    local image=$1
+    local container=${PWD##*/}
+    echo Creating container ${container} from image ${image}
+    lxc launch ${image} ${container}
+    lxc config set ${container} raw.idmap "both $(id -u) $(id -g)"
+    lxc restart ${container}
+    echo Container ${container} has been created and started.
+    echo Use lxw status to get the IP and add it to your hosts file,'if' required!
+}
+
+# Start a container, using the current folder name as container name.
+start(){
+    local container=${PWD##*/}
+    echo Starting container ${container}
+    lxc start ${container}
+    echo Container ${container} has been started.
+}
+
+# Restart a container, using the current folder name as container name.
+restart(){
+    local container=${PWD##*/}
+    echo Restarting container ${container}
+    lxc restart ${container}
+    echo Container ${container} has been restarted.
+}
+
+# Shutdown a container, using the current folder name as container name.
+stop(){
+    local container=${PWD##*/}
+    echo Stopping container ${container}
+    lxc stop ${container}
+    echo Container ${container} has been stopped.
+}
+
+# Destroy a container, using the current folder name as container name.
+# Command needs to be confirmed, running containers are stopped before deletion.
+destroy(){
+    local container=${PWD##*/}
+    echo Killing container ${container}
+    read -p "Continue (y/n)?" choice
+    case "$choice" in
+      y|Y ) delete;;
+      * ) exit;;
+    esac
+}
+
+delete(){
+    local container=${PWD##*/}
+    lxc delete ${container} -f
+    echo Container ${container} has been removed.
+    echo Clean up your hosts file, 'if' required!
+}
+
+# Open a shell inside a container, using the current folder name as container name.
+# We don't know the current shell, so we use a little su tweak here.
+shell(){
+    local container=${PWD##*/}
+    echo Entering container ${container}
+    lxc exec ${container} su -
+}
+
+# Mount a host folder inside the container, using the current folder name as container name.
+# We need to specify an alias for the mount, the (absolute or relative) path on the host,
+# and the absolute path inside the container.
+mnt(){
+    local device=$1
+    local source=$(readlink -e $2)
+    local target=$3
+    local container=${PWD##*/}
+    echo Mounting device ${device} to container ${container}: ${source} "=>" ${target}
+    lxc config device add ${container} ${device} disk source=${source} path=${target}
+    echo Folder has been mounted.
+}
+
+# Unmount a shared folder, using the current folder name as container name, and the alias
+unmnt(){
+    local device=$1
+    local container=${PWD##*/}
+    echo Unmounting device ${device} from container ${container}
+    lxc config device remove ${container} ${device}
+    echo Folder has been unmounted.
+}
+
+# Show the current status of the container (e.g. the IP)
+status(){
+    local container=${PWD##*/}
+    lxc list ${container}
+}
+
+
+# Show usage instructions
+usage(){
+    echo "LXW is a simple wrapper for LXD cli commands"
+    echo ""
+    echo "Usage: lxw <command>"
+    echo ""
+    echo "Commands:"
+    echo "  create <image>                           - Create a container using an image"
+    echo "  start                                    - Start the container"
+    echo "  restart                                  - Restart the container"
+    echo "  stop                                     - Stop the container"
+    echo "  kill                                     - Remove the container (needs confirmation)"
+    echo "  mount <alias> <host_dir> <container_dir> - Share a host folder with container"
+    echo "  unmount <alias>                          - Stop sharing a host folder with container"
+    echo "  shell                                    - Open a shell inside the container"
+    echo "  status                                   - Show current container status"
+    echo "  help                                     - Show usage instructions"
+}
+
+# We need at least one argument
+if [ $# -eq 0 ]; then
+   usage
+   exit 1
+fi
+
+# Run the command given as argument
+case "$1" in
+  "create")
+    if [ $# -ne 2 ]; then
+      usage
+      exit 1;
+    fi
+    create $2
+    ;;
+  "start")
+    start
+    ;;
+  "restart")
+    restart
+    ;;
+  "stop")
+    stop
+    ;;
+  "kill")
+    destroy
+    ;;
+  "mount")
+    if [ $# -ne 4 ]; then
+      usage
+      exit 1;
+    fi
+    mnt $2 $3 $4
+    ;;
+  "unmount")
+    if [ $# -ne 2 ]; then
+      usage
+      exit 1;
+    fi
+    unmnt $2
+    ;;
+  "shell")
+    shell
+    ;;
+  "status")
+    status
+    ;;
+  "help")
+    usage
+    ;;
+  *)
+    usage
+    ;;
+esac
